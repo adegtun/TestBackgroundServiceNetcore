e7c92f59e877f42ec121e2bc57dde1a03ba078d0
Temitope <temitope.adegunle@ubagroup.com>
2020-04-20 13:10:26 +0100

    Initial Commit

---
+++ BkgrndApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BkgrndApp.Models;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace BkgrndApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMemoryCache memoryCache;
+        public HomeController(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+        public string Message { get; set; }
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        public void OnGet()
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BkgrndApp/UdpListener.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BkgrndApp
+{
+    public class UdpListener : BackgroundService
+    {
+        private UdpClient client;
+        private readonly IMemoryCache memoryCache;
+
+        public UdpListener(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            client = new UdpClient(9000);
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var result = await client.ReceiveAsync();
+                    var message = Encoding.ASCII.GetString(result.Buffer);
+                    memoryCache.Set("UdpMessage", message);
+                }
+                catch (Exception ex)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(3), stoppingToken);
+                }
+            }
+        }
+
+        public override void Dispose()
+        {
+            if(client != null)
+            {
+                client.Dispose();
+            }
+            base.Dispose(); 
+        }
+    }
+}
